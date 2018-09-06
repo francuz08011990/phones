7743a9bcf532087a6a11e0bf6c9d8773f2f16c4f
Gennadiy Domoshenko <francuz08011990@gmail.com>
2018-09-06 16:06:16 +0300

    add to admin: search and filter

--- models/admin.py
+++ models/admin.py
@@ -3,5 +3,18 @@ from django.contrib import admin
 from .models import Phones, Type
 
 
-admin.site.register(Phones)
-admin.site.register(Type)
+@admin.register(Phones)
+class PhonesAdmin(admin.ModelAdmin):
+    search_fields = ('brand',)
+    list_filter = ('brand', )
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    fields = (
+        'name_of_model', 'phone', 'price', 'image'
+    )
+    list_display = ('id', 'name_of_model', 'phone', 'price', )
+    search_fields = ('name_of_model', )
+
+
